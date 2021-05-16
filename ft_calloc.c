d565071579d3bd2440a4058b9f9af543885f4384
Jaeryong Song <jasong@c1r9s7.42seoul.kr>
2021-05-16 21:12:25 +0900

    calloc modified

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: jasong <jasong@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/05/07 14:34:33 by jasong            #+#    #+#             */
-/*   Updated: 2021/05/15 22:41:18 by jasong           ###   ########.fr       */
+/*   Updated: 2021/05/16 16:57:30 by jasong           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,23 @@ void	*ft_calloc(size_t count, size_t size)
 	unsigned char	*tmp;
 	size_t			len;
 
-	len = size * count;
-	tmp = (unsigned char *)malloc(len);
-	if (tmp == NULL)
-		return (NULL);
-	if (!len)
-		ft_bzero(tmp, 1);
-	else
+	if (size && count)
+	{
+		len = count * size;
+		if ((len / count != size) || (len % size != 0))
+			return (NULL);
+		tmp = (unsigned char *)malloc(len);
+		if (tmp == NULL)
+			return (NULL);
 		ft_bzero(tmp, size * count);
-	return ((void *)tmp);
+		return ((void *)tmp);
+	}
+	else
+	{
+		tmp = (unsigned char *)malloc(1);
+		if (!tmp)
+			return (NULL);
+		ft_bzero(tmp, 1);
+		return (tmp);
+	}
 }
